f09e25e496fa8010c35b0633e125370e0f41168a
Azimbek <omar.azimbek@gmail.com>
2023-12-30 00:38:32 +0600

    0.4.3 Commit

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,18 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
-    public void calculateSalary(int salary) {
+    public static void calculateSalary(int salary) {
+        if (salary>0&&salary<=10000) {
+            System.out.println(salary-(salary*0.15));
+        } else if (salary>10000 && salary<=20000) {
+            System.out.println(salary-(salary*0.18));
+        } else if (salary>20000) {
+            System.out.println(salary-(salary*0.2));
+        } else if (salary<=0) {
+            System.out.println("wrong input!");
+        }
+    }
+    public static void main(String[] args) {
+        calculateSalary(-200);
     }
 }
